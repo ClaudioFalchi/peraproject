34088c5aa5ff6a22678891cf66d626f3124152ba
ClaudioFalchi <claudio.falchi280@gmail.com>
2025-06-18 16:25:53 +0200

    version 18/06
    Aggiunta la pagina carrello e checkout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CartComponent } from './cart/cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
 
 const routes: Routes = [
   {path:'', component: HomeComponent},
-  {path:'cart', component: CartComponent}
-];
+  {path:'cart', component: CartComponent},
+  {path:'checkout', component: CheckoutComponent}
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-checkout',
-  templateUrl: './checkout.component.html',
-  styleUrls: ['./checkout.component.css']
+  templateUrl: './checkout.component.html'
 })
 export class CheckoutComponent {
+  checkoutForm = this.fb.group({
+    name: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    address: ['', Validators.required],
+    city: ['', Validators.required],
+    zip: ['', Validators.required],
+    card: ['', [Validators.required, Validators.pattern('^[0-9]{16}$')]]
+  });
 
+  submitted = false;
+
+  constructor(private fb: FormBuilder) {}
+
+  onSubmit() {
+    this.submitted = true;
+    if (this.checkoutForm.valid) {
+      // Qui puoi gestire l'invio dei dati
+      alert('Ordine inviato!');
+    }
+  }
 }
