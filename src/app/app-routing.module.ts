16cf32740cb6fecd688526600cc2529e6751a289
gabrielecerbioni <gabriele.cerbioni2007@gmail.com>
2025-06-19 09:43:54 +0200

    home+collegamenti

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CartComponent } from './cart/cart.component';
+import { LoginComponent} from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
   {path:'', component: HomeComponent},
-  {path:'cart', component: CartComponent}
+  {path:'cart', component: CartComponent},
+  {path:'login', component:LoginComponent},
+  {path:'register', component:RegisterComponent}
 ];
 
 @NgModule({
